18f1135632969298dccf8ef7d772c51385fa76d9
vinnyson <vincent.lim@tzapac.com>
2020-12-09 18:59:58 +0800

    Added Config

--- Contracts/Oracle/storage.js
+++ Contracts/Oracle/storage.js
@@ -1,11 +1,13 @@
 
+import conf from '../../conf/conf.js';
+
 export default {
     "prim": "Pair",
     "args": [
       {
         "prim": "Pair",
         "args": [
-          { "prim": "Pair", "args": [ { "string": "KT1Q4jEteeKTsU2itpXithzD3evidxnUxi5C" }, { "string": "tz1hdQscorfqMzFqYxnrApuS5i6QSTuoAp3w" } ] },
+          { "prim": "Pair", "args": [ { "string": "KT1Q4jEteeKTsU2itpXithzD3evidxnUxi5C" }, { "string": conf.adminAddress } ] },
           { "prim": "Pair", "args": [ { "string": "testing123" }, { "prim": "Pair", "args": [ { "int": "2" }, [] ] } ] }
         ]
       },
