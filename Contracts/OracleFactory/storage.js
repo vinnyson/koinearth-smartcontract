c2040abde19fb2e94ec03833feefbc53e6feee87
vinnyson <vincent.lim@tzapac.com>
2020-12-16 15:51:45 +0800

    add oracleFactory js code

---
+++ Contracts/OracleFactory/storage.js
@@ -0,0 +1,3 @@
+import conf from '../../conf/conf.js';
+
+export default { "prim": "Pair", "args": [ { "string": "KT1Q4jEteeKTsU2itpXithzD3evidxnUxi5C" }, { "prim": "Pair", "args": [ [], { "string": "tz1hdQscorfqMzFqYxnrApuS5i6QSTuoAp3w" } ] } ] }
